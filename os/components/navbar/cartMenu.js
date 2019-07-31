7ee8750be05e94d7a0d76f373fbdddb9b11239fc
Yvan Pangilinan <yvan@organicstart.com>
2019-07-31 13:32:39 -0400

    added cart items in cartMenu.js

--- os/components/navbar/cartMenu.js
+++ os/components/navbar/cartMenu.js
@@ -29,38 +29,72 @@ export function cartMenu() {
     $.get("http://localhost:3000/api/storefront/cart")
       .then(data => {
         const tempData = [];
-        tempData.push({
-          lineItems: {
-            physicalItems: [
-              {
-                brand: "Hipp",
-                couponAmount: 0,
-                discountAmount: 0,
-                discounts: [],
-                extendedListPrice: 36.99,
-                extendedSalePrice: 36.99,
-                giftWrapping: null,
-                id: "3bdc6b30-0509-4ec4-88e2-b3270868278a",
-                imageUrl:
-                  "https://cdn11.bigcommerce.com/s-od28hnj6la/products/97/images/294/hipp-kindermilch-1__23582__15457.1517412804.190.285.jpg?c=2",
-                isShippingRequired: true,
-                isTaxable: true,
-                listPrice: 36.99,
-                name:
-                  "HiPP 1+ Years Combiotic Childrens' Milk (Kindermilch) Formula (600g)",
-                parentId: null,
-                productId: 97,
-                quantity: 1,
-                salePrice: 36.99,
-                sku: "HP1P",
-                type: "physical",
-                url:
-                  "https://sandbox54.mybigcommerce.com/hipp-1-plus-combiotic-milk-formula/",
-                variantId: 69
-              }
-            ]
+        tempData.push(
+          {
+            lineItems: {
+              physicalItems: [
+                {
+                  brand: "Hipp",
+                  couponAmount: 0,
+                  discountAmount: 0,
+                  discounts: [],
+                  extendedListPrice: 36.99,
+                  extendedSalePrice: 36.99,
+                  giftWrapping: null,
+                  id: "3bdc6b30-0509-4ec4-88e2-b3270868278a",
+                  imageUrl:
+                    "https://cdn11.bigcommerce.com/s-od28hnj6la/products/97/images/294/hipp-kindermilch-1__23582__15457.1517412804.190.285.jpg?c=2",
+                  isShippingRequired: true,
+                  isTaxable: true,
+                  listPrice: 36.99,
+                  name:
+                    "HiPP 1+ Years Combiotic Childrens' Milk (Kindermilch) Formula (600g)",
+                  parentId: null,
+                  productId: 97,
+                  quantity: 1,
+                  salePrice: 36.99,
+                  sku: "HP1P",
+                  type: "physical",
+                  url:
+                    "https://sandbox54.mybigcommerce.com/hipp-1-plus-combiotic-milk-formula/",
+                  variantId: 69
+                }
+              ]
+            }
+          },
+          {
+            lineItems: {
+              physicalItems: [
+                {
+                  brand: "Lebenswert",
+                  couponAmount: 0,
+                  discountAmount: 0,
+                  discounts: [],
+                  extendedListPrice: 89.97,
+                  extendedSalePrice: 89.97,
+                  giftWrapping: null,
+                  id: "4ba8760b-7081-49ff-bf84-6959be0b2a06",
+                  imageUrl:
+                    "https://cdn11.bigcommerce.com/s-od28hnj6la/products/86/images/681/lebenswert-stage-1__41188__49976.1563893757.190.285.png?c=2",
+                  isShippingRequired: true,
+                  isTaxable: true,
+                  listPrice: 29.99,
+                  name:
+                    "Lebenswert Stage 1 Organic (Bio) Infant Milk Formula (500g)",
+                  parentId: null,
+                  productId: 86,
+                  quantity: 3,
+                  salePrice: 29.99,
+                  sku: "LB-1",
+                  type: "physical",
+                  url:
+                    "https://sandbox54.mybigcommerce.com/lebenswert/stage-1/product",
+                  variantId: 66
+                }
+              ]
+            }
           }
-        });
+        );
         // if (data.length < 1) {
         //   $("#cartItems")[0].innerHTML = "your cart is empty";
         // } else {
@@ -73,16 +107,18 @@ export function cartMenu() {
 }
 
 function parseData(data) {
-  let htmlArray = ["<ul>"];
   console.log(data);
-  data[0].lineItems.physicalItems.map(el => {
-    htmlArray.push(
-      `<li><img src=${el.imageUrl} alt="product/>${el.brand} ${el.name} ${el.salePrice} x ${
-        el.quantity
-      }</li>`
-    );
+  let htmlArray = ["<ul>"];
+  data.map(x => {
+    x.lineItems.physicalItems.map(el => {
+      htmlArray.push(
+        `<li><img src=${el.imageUrl} alt="product"/> ${el.brand} ${el.name} ${
+          el.salePrice
+        } x ${el.quantity}</li>`
+      );
+    });
   });
   htmlArray.push("</ul>");
-  console.log(htmlArray);
+  // console.log(htmlArray);
   return htmlArray.join("");
 }
