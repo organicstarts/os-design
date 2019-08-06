c4a8930190b8014f8ca09ad5d774a0b8ee5ae6e6
Isaiah Marc Sanchez <isaiah.marc.sanchez@gmail.com>
2019-08-06 11:25:26 -0400

    Added express cors.

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
+const cors = require('cors'); 
+app.use(cors());
+
 app.use('/dist', express.static(`${__dirname}/dist/`));
 
 app.get('*', (req, res) => {
